10dd2ec359c3f75629d3e2df1bae280efc96246b
Janakiraman276 <147291424+Janakiraman276@users.noreply.github.com>
2024-04-08 18:24:06 +0530

    Update nodes.tf

--- nodes.tf
+++ nodes.tf
@@ -29,7 +29,7 @@ resource "aws_iam_role_policy_attachment" "nodes-AmazonEC2ContainerRegistryReadO
 }
 
 resource "aws_eks_node_group" "private-nodes" {
-  cluster_name    = aws_eks_cluster.eks_cluster.name
+  cluster_name    = aws_eks_cluster.eks-cluster.name
   node_group_name = "private-nodes"
   node_role_arn   = aws_iam_role.nodes.arn
 
